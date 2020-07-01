cc3e5b8fb68b046c0708a2265a7d9506f05febc2
Adam Cowley <github@adamcowley.co.uk>
2020-07-01 12:17:55 +0100

    Added fonts/ directory to build

--- gulp.d/tasks/build.js
+++ gulp.d/tasks/build.js
@@ -98,6 +98,7 @@ module.exports = (src, dest, preview) => () => {
       .src('css/site.css', { ...opts, sourcemaps })
       .pipe(postcss((file) => ({ plugins: postcssPlugins, options: { file } }))),
     vfs.src('font/*.{ttf,woff*(2)}', opts),
+    vfs.src('fonts/*.{ttf,woff*(2)}', opts),
     vfs
       .src('img/**/*.{gif,ico,jpg,png,svg}', opts)
       .pipe(
