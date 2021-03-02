0e3057a9f2039d28546a574bec5fdb37ecfd4792
Adam Cowley <github@adamcowley.co.uk>
2021-03-02 14:31:06 +0000

    Added role parameter to personalisation script

--- src/js/30-personalisation.js
+++ src/js/30-personalisation.js
@@ -5,11 +5,14 @@ import { getCookie } from './modules/cookies'
   'use strict'
 
   document.addEventListener('DOMContentLoaded', () => {
-    const url = document.location.href.split('?')[0]
+    const urlObject = new URL(window.location.href)
+
+    const url = urlObject.origin + urlObject.pathname
     const identity = getCookie('neo_identity')
     const gid = getCookie('_gid')
     const uetsid = getCookie('_uetsid')
+    const role = urlObject.searchParams.get('role')
 
-    postRequest('https://neo4j-personalisation.netlify.app/view', { url, identity, gid, uetsid }, undefined, () => {}, () => {})
+    postRequest('https://neo4j-personalisation.netlify.app/view', { url, identity, gid, uetsid, role }, undefined, () => {}, () => {})
   })
 })()
