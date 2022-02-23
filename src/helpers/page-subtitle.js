406575b304532acb3ded421397633c199df55d56
David Oliver <david.oliver@neo4j.com>
2022-02-23 09:13:12 +0000

    Update sitename to Neo4j Graph Data Platform

--- src/helpers/page-subtitle.js
+++ src/helpers/page-subtitle.js
@@ -2,7 +2,7 @@
 
 module.exports = (page) => {
   if (page.layout === 'landing' || page.layout === 'training-enrollment') {
-    // since the component title and the page title are often the same, we use 'Neo4j Graph Database Platform' to avoid redundancy.
+    // since the component title and the page title are often the same, we use 'Neo4j Graph Data Platform' to avoid redundancy.
     return ' - Neo4j Graph Database Platform'
   }
   if (page && page.component && page.component.title) {
