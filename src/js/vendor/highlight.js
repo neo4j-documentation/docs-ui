0f53804e83df5f47547922979f0599c4d6fc7fc1
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2023-02-13 06:55:28 +0100

    Revert highlight.js to 10.7 because of issue with callouts. Add callout examples in code blocks.

--- src/js/vendor/highlight.js
+++ src/js/vendor/highlight.js
@@ -52,6 +52,45 @@
   })
   // add an alias, use properties syntax highlighter on conf
   hljs.registerLanguage('conf', require('highlight.js/lib/languages/properties'))
-  hljs.registerLanguage('graphql', require('highlight.js/lib/languages/graphql'))
+  hljs.registerLanguage('graphql', function (hljs) {
+    return {
+      aliases: ['gql'],
+      keywords: {
+        keyword:
+        'query mutation subscription|10 type interface union scalar fragment|10 enum on ...',
+        literal: 'true false null',
+      },
+      contains: [
+        hljs.HASH_COMMENT_MODE,
+        hljs.QUOTE_STRING_MODE,
+        hljs.NUMBER_MODE,
+        {
+          className: 'type',
+          begin: '[^\\w][A-Z][a-z]',
+          end: '\\W',
+          excludeEnd: true,
+        },
+        {
+          className: 'literal',
+          begin: '[^\\w][A-Z][A-Z]',
+          end: '\\W',
+          excludeEnd: true,
+        },
+        { className: 'variable', begin: '\\$', end: '\\W', excludeEnd: true },
+        {
+          className: 'keyword',
+          begin: '[.]{2}',
+          end: '\\.',
+        },
+        {
+          className: 'meta',
+          begin: '@',
+          end: '\\W',
+          excludeEnd: true,
+        },
+      ],
+      illegal: /([;<']|BEGIN)/,
+    }
+  })
   hljs.highlightAll()
 })()
