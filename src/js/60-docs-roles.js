1b239f0f17a15c5884720f1bee32bbb0fd275dd9
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-02-27 13:47:08 +0000

    Show multiple roles on headers (#151)
    * Show multiple roles on headers
    
    * add show-roles class automatically

---
+++ src/js/60-docs-roles.js
@@ -0,0 +1,19 @@
+import { createElement } from './modules/dom'
+
+document.addEventListener('DOMContentLoaded', function () {
+  const roleDivs = document.querySelectorAll('body.docs div[class^="sect"]:not(.sectionbody)')
+  roleDivs.forEach(function (roleDiv) {
+    var roles = roleDiv.classList
+    roles = [...roles].filter(function (c) {
+      return (!c.startsWith('sect'))
+    })
+    if (roles) {
+      var insert = createElement('div', 'roles')
+      roles.forEach(function (role) {
+        insert.append(createElement('span', 'role ' + role))
+      })
+      roleDiv.firstElementChild.after(insert)
+      roleDiv.classList.add('show-roles')
+    }
+  })
+})
