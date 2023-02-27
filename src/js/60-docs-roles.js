07d60e4d90313e039201e8a6bc00f41d69ea087f
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-02-27 11:03:52 +0000

    add show-roles class automatically

--- src/js/60-docs-roles.js
+++ src/js/60-docs-roles.js
@@ -1,14 +1,19 @@
 import { createElement } from './modules/dom'
 
 document.addEventListener('DOMContentLoaded', function () {
-  const roleDivs = document.querySelectorAll('body.docs div.show-roles')
+  const roleDivs = document.querySelectorAll('body.docs div[class^="sect"]:not(.sectionbody)')
   roleDivs.forEach(function (roleDiv) {
     var roles = roleDiv.classList
-    var insert = createElement('div', 'page-roles')
-    roles.forEach(function (role) {
-      if (role === 'show-roles' || role.startsWith('sect')) return
-      insert.append(createElement('span', 'page-role ' + role))
+    roles = [...roles].filter(function (c) {
+      return (!c.startsWith('sect'))
     })
-    roleDiv.firstElementChild.after(insert)
+    if (roles) {
+      var insert = createElement('div', 'page-roles')
+      roles.forEach(function (role) {
+        insert.append(createElement('span', 'page-role ' + role))
+      })
+      roleDiv.firstElementChild.after(insert)
+      roleDiv.classList.add('show-roles')
+    }
   })
 })
