28f5fa0c520e919471ea1ba7b1b19345f50bda7b
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-03-02 10:16:15 +0000

    Section roles fix (#152)
    * Flex roles to the right, like the cheat-sheet

--- src/js/60-docs-roles.js
+++ src/js/60-docs-roles.js
@@ -1,19 +1,38 @@
 import { createElement } from './modules/dom'
 
 document.addEventListener('DOMContentLoaded', function () {
-  const roleDivs = document.querySelectorAll('body.docs div[class^="sect"]:not(.sectionbody)')
-  roleDivs.forEach(function (roleDiv) {
-    var roles = roleDiv.classList
-    roles = [...roles].filter(function (c) {
+  const sectionDivs = document.querySelectorAll('body.docs div[class^="sect"]:not(.sectionbody,.sect-header)')
+  sectionDivs.forEach(function (sectionDiv) {
+    var roles = sectionDiv.classList
+    roles = [...roles].sort().filter(function (c) {
       return (!c.startsWith('sect'))
     })
-    if (roles) {
+
+    var newRolesDiv = createElement('div', 'sect-header')
+    var head = sectionDiv.querySelector('h2,h3,h4,h5,h6')
+    // console.log(head.tagName)
+    newRolesDiv.append(head)
+
+    if (roles.length > 0) {
       var insert = createElement('div', 'roles')
       roles.forEach(function (role) {
-        insert.append(createElement('span', 'role ' + role))
+        insert.append(createElement('span', `role ${role}`))
+
+        // const span = createElement('span', `label label--${role}`)
+
+        // // let span = createElement('span', `role ${role}`)
+        // // console.log(span.outerHTML)
+
+        // let elem = document.querySelector(`span.${role}`)
+
+        // // let elem = getComputedStyle(span, ':after').content;
+        // span.textContent = getComputedStyle(elem, ':after').getPropertyValue('content').replace(/"/g, '')
+        // console.log(span.textContent)
+        // insert.append(span)
       })
-      roleDiv.firstElementChild.after(insert)
-      roleDiv.classList.add('show-roles')
+      newRolesDiv.append(insert)
     }
+    sectionDiv.prepend(newRolesDiv)
+    sectionDiv.classList.add('show-roles')
   })
 })
