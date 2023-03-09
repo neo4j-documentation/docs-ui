1019637e150e4507701c5e9f4183b5cbe45c3ce5
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-03-09 16:05:30 +0000

    Fix header padding (#156)

--- src/js/60-docs-roles.js
+++ src/js/60-docs-roles.js
@@ -8,31 +8,20 @@ document.addEventListener('DOMContentLoaded', function () {
       return (!c.startsWith('sect'))
     })
 
+    if (roles.length === 0) return
+
     var newRolesDiv = createElement('div', 'sect-header')
     var head = sectionDiv.querySelector('h2,h3,h4,h5,h6')
-    // console.log(head.tagName)
-    newRolesDiv.append(head)
 
     if (roles.length > 0) {
+      newRolesDiv.append(head)
       var insert = createElement('div', 'roles')
       roles.forEach(function (role) {
         insert.append(createElement('span', `role ${role}`))
-
-        // const span = createElement('span', `label label--${role}`)
-
-        // // let span = createElement('span', `role ${role}`)
-        // // console.log(span.outerHTML)
-
-        // let elem = document.querySelector(`span.${role}`)
-
-        // // let elem = getComputedStyle(span, ':after').content;
-        // span.textContent = getComputedStyle(elem, ':after').getPropertyValue('content').replace(/"/g, '')
-        // console.log(span.textContent)
-        // insert.append(span)
       })
       newRolesDiv.append(insert)
+      sectionDiv.prepend(newRolesDiv)
+      sectionDiv.classList.add('show-roles')
     }
-    sectionDiv.prepend(newRolesDiv)
-    sectionDiv.classList.add('show-roles')
   })
 })
