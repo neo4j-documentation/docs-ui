baeb8b4732c44ba20454d38e34b3016e202913fd
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-04-21 09:13:55 +0100

    Update page title

--- src/js/50-cheat-sheet-toggle.js
+++ src/js/50-cheat-sheet-toggle.js
@@ -33,7 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   if (selectionFromPath) {
     updateSelectorFromProduct(selectionFromPath)
-    updateOgFromProduct(selectionFromPath, curURL)
+    updateMetaFromProduct(selectionFromPath, curURL)
   }
 
   // check for a checkbox to display or hide labels
@@ -345,14 +345,18 @@ function updateSelectorFromProduct (product) {
   }
 }
 
-function updateOgFromProduct (product, curURL) {
+function updateMetaFromProduct (product, curURL) {
   const ogDesc = document.querySelector('meta[property="og:description"]')
+  const ogTitle = document.querySelector('meta[property="og:title"]')
   const ogUrl = document.querySelector('meta[property="og:url"]')
+
   const text = getProductFromOptionMap(product)
   const ogDescFromProduct = 'Cypher Cheat Sheet - ' + text
 
   ogUrl.setAttribute('content', curURL)
   ogDesc.setAttribute('content', ogDescFromProduct)
+  ogTitle.setAttribute('content', ogDescFromProduct)
+  document.title = ogDescFromProduct
 }
 
 const stripTrailingSlash = (str) => {
