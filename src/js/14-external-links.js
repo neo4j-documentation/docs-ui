77105235f22a9ecf73455415934cc957ccb449b7
Neil Dewhurst <neil.dewhurst@neo4j.com>
2023-12-08 14:16:25 +0000

    a11y updates

--- src/js/14-external-links.js
+++ src/js/14-external-links.js
@@ -4,5 +4,6 @@
   document.querySelectorAll('.doc a[href^="http"], .doc a[target="_blank"]')
     .forEach(function (el) {
       el.classList.add('external')
+      el.setAttribute('aria-label', 'Link label (opens in new tab)')
     })
 })()
