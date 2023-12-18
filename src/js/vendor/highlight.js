3e7910fdf32f182d5cb63f681dc2db18c8ce2448
Stefano Ottolenghi <stefano.ottolenghi@neo4j.com>
2023-12-18 09:59:13 +0100

    Allow use of `// mark-line` to highlight lines in code blocks.

--- src/js/vendor/highlight.js
+++ src/js/vendor/highlight.js
@@ -128,4 +128,12 @@
       }
     })
   hljs.highlightAll()
+
+  // Apply line highlighting to lines marked with `// marked-line`
+  // eat away line breaks or they would be doubled by the div
+  hljs.addPlugin({
+    'after:highlightElement': ({ el, result, text }) => {
+      result.value = result.value.replaceAll(/^(\s*)(.+?)\s*<span class="hljs-comment">.*?\bmark-line\b.*?<\/span>\n/mg, '<div class="highlight-line">$1$2</div>')
+    }
+  });
 })()
