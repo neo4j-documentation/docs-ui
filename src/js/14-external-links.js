8e44beb7416f54dfc725d7141fd3f9f644256787
Neil Dewhurst <ndewhurst@gmail.com>
2024-07-01 11:39:02 +0100

    Better external links

--- src/js/14-external-links.js
+++ src/js/14-external-links.js
@@ -1,8 +1,10 @@
 ;(function () {
   'use strict'
-
+  const nonExternals = /neo4j\.[com|dev]/g
   document.querySelectorAll('.doc a[href^="http"], .doc a[target="_blank"]')
     .forEach(function (el) {
-      el.classList.add('external')
+      if (document.location.hostname !== el.hostname && !el.hostname.match(nonExternals)) {
+        el.classList.add('external')
+      }
     })
 })()
