c2244ed6d7069869d90b9c13d11ca47459e6d4bc
Neil Dewhurst <ndewhurst@gmail.com>
2024-07-03 17:31:55 +0100

    selectors

--- src/js/14-external-links.js
+++ src/js/14-external-links.js
@@ -1,3 +1,10 @@
+// external links
+// link:https://example.com[text^] - add class external and aria-label (already has target="_blank")
+// link:https://example.com[text] - do nothing
+// links to other docs pages are not external
+// link:https://neo4j.com/docs/...[] - do nothing
+// link:{neo4j-docs-bas-uri}/...[] - do nothing
+
 ;(function () {
   'use strict'
   const nonExternals = /neo4j\.[com|dev]/g
@@ -5,6 +12,11 @@
     .forEach(function (el) {
       if (document.location.hostname !== el.hostname && !el.hostname.match(nonExternals)) {
         el.classList.add('external')
+        el.setAttribute('aria-label', 'Link label (opens in new tab)')
+      }
+
+      if (el.target === '_blank') {
+        el.setAttribute('aria-label', 'Link label (opens in new tab)')
       }
     })
 })()
