neo4j-documentation/docs-ui

commit f4dec1df5c79e4666729d96e3fc956ed2418afb2

Add beta-until label (#254)
For new and deprectaed labels it is already possible to include a
version number. Although we have a generic `beta` label that can be used
to display just the text `Beta`, there is no way to display a version
with the text, for example `Beta until 5.12`.

This PR adds a new `label--beta-until-<version>` role to be used for
beta labels where a version number is also required.

src/js/60-docs-roles.js
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // label could be eg aura-db-enterprise - we use the full label
     // label could be eg new-5.20 - we use 'new' for the label and add the version as text
-    label = (rolesData[label] && rolesData[label].category !== 'version') ? label : labelParts[0]
+    label = (rolesData[label] && rolesData[label].labelCategory !== 'version') ? label : labelParts.slice(0, -1).join('-')
 
     // ignore labels that are not defined in rolesData
     if (!rolesData[label]) {
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // get version number for version labels
     if ((rolesData[label].labelCategory === 'version' || rolesData[label].versionText) && labelParts[1]) {
-      labelDetails.data.version = labelParts[1]
+      labelDetails.data.version = labelParts.pop()
       const joinText = rolesData[label].versionText ? rolesData[label].versionText : 'in'
       labelDetails.text = [labelDetails.text, joinText, labelDetails.data.version].join(' ')
     }

src/css/labels.css
@@ -167,7 +167,8 @@ span.label--yes {
 }
 
 span.label--alpha,
-span.label--beta {
+span.label--beta,
+span.label--beta-until {
   background: var(--alpha-beta-background-color);
   color: var(--alpha-beta-color);
 }

commit c70053723d260af2ff0fe6327a173514b7ca7adf

Deprecated fix (#257)
Fixes a bug where `role=label--deprecated` was not being displayed

eg
https://neo4j.com/docs/cypher-manual/current/subqueries/call-subquery/#importing-with

Found a bug introduced in #254 that was treating deprecated as a role
that required a version to be specified. Deprecated can be used with or
without a version number.

src/js/60-docs-roles.js
@@ -25,9 +25,19 @@ document.addEventListener('DOMContentLoaded', function () {
     var label = role.replace('label--', '')
     var labelParts = label.split('-')
 
-    // label could be eg aura-db-enterprise - we use the full label
-    // label could be eg new-5.20 - we use 'new' for the label and add the version as text
-    label = (rolesData[label] && rolesData[label].labelCategory !== 'version') ? label : labelParts.slice(0, -1).join('-')
+    // roles can be single word ie beta - use beta as label class and text from rolesDatee.beta
+    // roles can be single word + version ie new-5.20 - use new as label class and text from rolesData.new + version number
+    // roles can be multiple words ie aura-db-enterprise - use aura-db-enterprise as label class and text from rolesData.aura-db-enterprise
+    // roles like deprecated can appear with or without a version number - deprecated-5.20 or deprecated
+    // - use deprecated as label class and text from rolesData.deprecated
+    // - use deprecated as label class and text from rolesData.deprecated + version number
+
+    // so if the role is a single word, we use the role as is - ie deprecated
+    // if it is longer we test to see if it is a 'versionable' roke - ie deprecated-5.20
+    // if it is a versionable role, and a veresion is specified, we remove the version and use the remaining text as the label class
+    if (labelParts.length > 1) {
+      label = (rolesData[label] && rolesData[label].labelCategory !== 'version') ? label : labelParts.slice(0, -1).join('-')
+    }
 
     // ignore labels that are not defined in rolesData
     if (!rolesData[label]) {