b92b16bd3c1cd266b8119cfb80be734383880283
Neil Dewhurst <ndewhurst@gmail.com>
2025-03-18 21:21:56 +0000

    if no latest use page version if set

--- src/helpers/neo-canonical-current.js
+++ src/helpers/neo-canonical-current.js
@@ -4,10 +4,14 @@
 // we want to modify the canonical by replacing that version with 'current'
 // we can allow for a page attribute to be set to override 'current'
 
+// if there's only one version of the page, there is no page.latest
+// if the version is ~ there is no version in the path
+// if both of these are true, we return the canonical URL as is
+
 'use strict'
 
 module.exports = (page) => {
-  if (!page.latest && !page.attributes['use-current-mapping']) return page.canonicalUrl
+  if (!page.latest && !page.version) return page.canonicalUrl
   const versionToReplace = page.latest ? page.latest.version : page.version
   const re = new RegExp(`/${versionToReplace}/`)
   const latestVersionPath = `/${(page.attributes['latest-version-path'] || 'current')}/`
