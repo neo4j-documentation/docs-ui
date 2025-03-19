1e55d0cf593fd5ff690d5216617c5d141e80a12f
Neil Dewhurst <neil.dewhurst@neo4j.com>
2025-03-19 12:00:28 +0000

    Replace version with current if single version and version is defined (#307)
    Use `page.latest` or `page.version` to get a version to replace with
    `current`.
    
    If the component has a single version there is no `page.latest`. If
    `version: ~` in antora.yml there is no page.version.
    
    If there's no version to replace, return the canonicalUrl as is.

--- src/helpers/neo-canonical-current.js
+++ src/helpers/neo-canonical-current.js
@@ -4,11 +4,15 @@
 // we want to modify the canonical by replacing that version with 'current'
 // we can allow for a page attribute to be set to override 'current'
 
+// if the version is ~ there is no version in the path - we return the canonical URL as is
+// if there's only one version of the page, there is no page.latest - we use page.version
+
 'use strict'
 
 module.exports = (page) => {
-  if (!page.latest) return page.canonicalUrl
-  const re = new RegExp(`/${page.latest.version}/`)
+  const versionToReplace = page.latest ? page.latest.version : page.version
+  if (!versionToReplace) return page.canonicalUrl
+  const re = new RegExp(`/${versionToReplace}/`)
   const latestVersionPath = `/${(page.attributes['latest-version-path'] || 'current')}/`
   return page.canonicalUrl.replace(re, latestVersionPath)
 }
