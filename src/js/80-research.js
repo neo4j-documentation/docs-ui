6d5fb67eed3cb7f6f14ea7a639674c771c485ee2
lidiazuin <lidiazuin@gmail.com>
2025-03-20 11:15:44 +0100

    removing js files not used anymore

--- src/js/80-research.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.faq-question').forEach(item => {
-  item.addEventListener('click', () => {
-      const parent = item.parentElement;
-      parent.classList.toggle('active');
-      const arrow = item.querySelector('.arrow');
-      arrow.style.transform = parent.classList.contains('active') ? 'rotate(90deg)' : 'rotate(0deg)';
-  });
-});
