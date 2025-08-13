0f4c59ca12a1533c85ea175dc9f50f0ed2f2755a
Mark Woulfe <82045391+MarkWoulfeNeo4j@users.noreply.github.com>
2025-08-13 15:40:42 +0100

    Added page query parameter to scarf tracking pixel (#339)

--- src/js/no-lint/31-scarf-pixel.js
+++ src/js/no-lint/31-scarf-pixel.js
@@ -2,7 +2,7 @@
 window.addEventListener('CookiebotOnAccept', function () {
   if (Cookiebot.consent.marketing) {
     var img = document.createElement('img')
-    img.src = 'https://static.neo4j.com/a.png?x-pxid=dae9b690-6a56-4889-a951-9d0207a18da6'
+    img.src = 'https://static.neo4j.com/a.png?x-pxid=dae9b690-6a56-4889-a951-9d0207a18da6&page=' + encodeURIComponent(window.location.href)
     img.referrerPolicy = 'no-referrer-when-downgrade'
     img.style.display = 'none'
     document.body.appendChild(img)
