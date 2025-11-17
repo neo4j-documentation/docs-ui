8bc09a8436ccec924dcfc1a266fe4b32aebf94b5
Neil Dewhurst <ndewhurst@gmail.com>
2025-11-17 18:14:10 +0000

    use unless

--- src/js/60-docs-roles.js
+++ src/js/60-docs-roles.js
@@ -140,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // no need to do anything if we found only undefined roles
     if (labels.length === 0) return
 
-    let labelsLocation = (roleDiv.firstElementChild && headings.includes(roleDiv.firstElementChild.nodeName)) ? roleDiv.firstElementChild : roleDiv
+    // let labelsLocation = (roleDiv.firstElementChild && headings.includes(roleDiv.firstElementChild.nodeName)) ? roleDiv.firstElementChild : roleDiv
     const labelsDiv = createElement('div', 'labels')
 
     for (const label of labels) {
@@ -156,9 +156,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     console.log(roleDiv.classList)
 
-    if (roleDiv.classList.contains('admonitionblock')) {
-      labelsLocation = roleDiv.querySelector('td.content')
-    }
+    // if (roleDiv.classList.contains('admonitionblock')) {
+    //   labelsLocation = roleDiv.querySelector('td.content')
+    // }
 
     if (roleDiv.nodeName === 'H1' || headings.includes(roleDiv.firstElementChild.nodeName)) {
       // labelsLocation.append(labelsDiv)
