84057b91668408e12ef3c718ad7f7c768fb24fc2
Neil Dewhurst <ndewhurst@gmail.com>
2025-12-19 13:23:30 +0000

    91%?

--- src/js/vendor/image-zoom.js
+++ src/js/vendor/image-zoom.js
@@ -4,9 +4,9 @@ import mediumZoom from 'medium-zoom'
 const zoomOptions = {
   container: {
     top: 120,
-    bottom: 10
+    bottom: 10,
   },
-  background: 'rgba(33, 35, 37, 1)'
+  background: 'rgba(33, 35, 37, 1)',
 }
 mediumZoom('.imageblock > .content > img', zoomOptions)
 mediumZoom('.image.popup-link > img', zoomOptions)
