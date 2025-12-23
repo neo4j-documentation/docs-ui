e949485bd53cab1adfc7cddb102ad34bd4c683f6
Neil Dewhurst <ndewhurst@gmail.com>
2025-12-23 17:45:17 +0000

    buttons and stuff

--- src/js/00-theme.js
+++ src/js/00-theme.js
@@ -3,8 +3,8 @@
 
   // set the initial color scheme based on user preference or to system
   const themeMenu = document.getElementById('theme-dropdown')
-
   const themeItems = themeMenu.querySelectorAll('.navbar-item')
+  const logos = document.querySelectorAll('.navbar-logo')
 
   document.addEventListener('DOMContentLoaded', function () {
     const userColorSchemeName = 'user-color-scheme-preference'
@@ -25,12 +25,10 @@
       ? window.matchMedia('(prefers-color-scheme: dark)') : userColorScheme === 'dark'
         ? { matches: true, addEventListener: () => {} } : { matches: false, addEventListener: () => {} }
 
-    function handleThemeChange (e) {
-      const isDark = e.matches // true if system is dark, false if light
-      document.documentElement.style.colorScheme = isDark ? 'dark' : 'light'
-    }
-    handleThemeChange(darkMode)
-    darkMode.addEventListener('change', handleThemeChange)
+    const isDark = darkMode.matches // true if system is dark, false if light
+    document.documentElement.style.colorScheme = isDark ? 'dark' : 'light'
+    updateLogos(isDark ? 'dark' : 'light')
+    // darkMode.addEventListener('change', handleThemeChange)
   })
 
   // NOTE don't let event get picked up by window click listener
@@ -42,14 +40,32 @@
     const userColorSchemeName = 'user-color-scheme-preference'
     localStorage.setItem(userColorSchemeName, theme)
 
-    if (theme === 'light' || theme === 'dark') {
-      document.documentElement.style.colorScheme = theme
-    } else if (theme === 'system') {
-      const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-      document.documentElement.style.colorScheme = isDarkMode ? 'dark' : 'light'
-    }
+    const lightOrDark = resolveLightOrDark(theme)
 
+    document.documentElement.style.colorScheme = lightOrDark
+
+    // add a selected class to the selected theme item
     updateSelectedThemeItem(themeItems, theme)
+    updateLogos(lightOrDark)
+  }
+
+  function resolveLightOrDark (theme) {
+    if (theme === 'system') {
+      const isDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+      theme = isDarkMode ? 'dark' : 'light'
+    }
+    return theme
+  }
+
+  function updateLogos (theme) {
+    logos.forEach((logo) => {
+      const logoTheme = logo.getAttribute('data-theme')
+      if (logoTheme === theme) {
+        logo.classList.remove('hidden')
+      } else {
+        logo.classList.add('hidden')
+      }
+    })
   }
 
   function updateSelectedThemeItem (themeItems, theme) {
